65b9724114bd518940cbc0d785f68a3610d76ade
INOUE Takuya <inouetakuya5@gmail.com>
2011-10-09 16:40:28 +0900

    Emacs を独立して管理することにした .emacs.d >> ~/.emacs.d

--- .emacs.d/init.d/keybinds.el
+++
@@ -1,6 +0,0 @@
-;; キーバインド（グローバル）
-(global-set-key "\C-h" 'delete-backward-char)
-(global-set-key "\C-c>" 'comment-region)
-(global-set-key "\C-c<" 'uncomment-region)
-(global-set-key "\C-xg" 'goto-line)
-
